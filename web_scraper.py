7193918a702472ff4f4829b500b911ad0654c46b
Unknown <nigelejackson@gmail.com>
2017-03-09 21:26:23 -0500

    Started building web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,19 @@
+'''Basic Web Scraper
+
+first python web scrapper. following HTML Scraping
+from The Hitchhiker's Guide to Python'''
+
+from lxml import html
+import requests
+
+
+
+def main():
+    '''Main Function    
+
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
