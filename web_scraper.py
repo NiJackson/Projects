328ea9c600c29a6f4d1b341457b76e553ce659ab
Unknown <nigelejackson@gmail.com>
2017-03-09 23:03:56 -0500

    Work done on web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -3,17 +3,23 @@
 first python web scrapper. following HTML Scraping
 from The Hitchhiker's Guide to Python'''
 
+from sys import argv
 from lxml import html
 import requests
 
 
+def harvest(url):
+    '''scraper function
+
+    take url as command argument and pull page from url'''
+    page = requesits.get(url)
+    return
 
 def main():
-    '''Main Function    
+    '''Main Function
 
     '''
-    pass
-
+    harvest(argv[1])
 
 if __name__ == '__main__':
     main()
